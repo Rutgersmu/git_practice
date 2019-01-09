4ddb39d5f04c53c59389db000f4774f50e65114f
Rutgersmu <softstep1221@gmail.com>
2019-01-09 13:33:15 +0900

    first_commit

---
+++ 9-7.py
@@ -0,0 +1,13 @@
+in_file = open('chicken.txt', 'r')
+rev = 0
+day = 0
+for line in in_file:
+    data = line.strip().split(": ")
+    amount = int(data[1])
+    rev += amount
+    day += 1
+
+ave_rev = rev / day
+print(ave_rev)
+
+in_file.close()
